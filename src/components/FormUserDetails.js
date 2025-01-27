01b25a3d82d8dc3084f42531fdafd28fb6486e0e
webQbe <webcubesolutionslk@gmail.com>
2025-01-27 11:01:37 +0530

    Created AppBar in FormUserDetails

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+
+const theme = createTheme();
 
 export class FormUserDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello from user details</h1> 
-      </div>
+      <ThemeProvider theme={theme}>
+          <React.Fragment>
+            <AppBar position="static">
+              <h2>Enter User Details</h2>
+            </AppBar>
+          </React.Fragment>
+      </ThemeProvider>
     )
   }
 }
